c08341371fd0b5d09ebcea7aebce7b014413866b
stars62 <3539245698@qq.com>
2026-07-16 07:06:01 +0800

    feat: 更新了新的算法代码

--- Max_And_Next_Max_Number/Max_And_Next_Max_Number.cpp
+++ Max_And_Next_Max_Number/Max_And_Next_Max_Number.cpp
@@ -4,6 +4,38 @@ using namespace std;
 int main() {
 	int arr[20];
 	int num;
+	int max, smax;
 
+	cout << "Enter number of elements in array:" << endl;
+	cin >> num;
 
+	cout << "Enter your array" << endl;
+	for (int i = 0; i < num; i++) {
+		cin >> arr[i];
+	}
+
+	if (arr[0] > arr[1]) {
+		max = arr[0];
+		smax = arr[1];
+	}
+	else {
+		max = arr[1];
+		smax = arr[0];
+	}
+
+	for (int i = 2; i < num; i++) {
+		if (arr[i] > smax) {
+			if (arr[i] > max) {
+				max = smax;
+				max = arr[i];
+			}
+			else {
+				smax = arr[i];
+			}
+		}
+	}
+	cout << "Max:" << endl;
+	cout << "Next max:" << endl;
+
+	return 0;
 }
